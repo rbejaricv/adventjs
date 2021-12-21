6f753ecbd18ca2776821e655d6fcd752b87edc41
Romà Bejar <roma.bejar@iescarlesvallbona.cat>
2021-12-21 20:09:27 +0100

    Remove extra stuff from skeleton

--- 00-skeleton.js
+++ 00-skeleton.js
@@ -1,14 +1,5 @@
-const carta = 'bici coche balón _playstation bici coche peluche'
-
-function not_crossed(gift) { return !gift[0] == '_'; }
-
-export default function listGifts(letter) {
-    letter = letter.trim().split(" ").filter(not_crossed);
-    return {}
-}
-
 function main() {
-    console.log(listGifts(carta));
+    console.log();
 }
 
 if (require.main === module) {
